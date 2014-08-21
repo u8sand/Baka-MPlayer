4c22100bf0fd9aed957ffc9611ac7ab34c42d3cb
Daniel Clarke <u8sand@gmail.com>
2014-08-20 22:14:04 -0400

    Using src/ instead of source/ Also adjusted manual a bit.

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ hash make 2>/dev/null || { echo >&2 "Cannot find make."; exit 1; }
 qmake_ver=`qmake --version | awk '/Using Qt version.*/{print $4}'`
 echo "Found qt version $qmake_ver...";
 if [[ $qmake_ver =~ (5.*) ]]; then
-	qmake source/Baka-MPlayer.pro && make $@
+	qmake src/Baka-MPlayer.pro && make $@
 else
 	echo "You need qt version 5.x.x to compile Baka MPlayer.";
 fi
