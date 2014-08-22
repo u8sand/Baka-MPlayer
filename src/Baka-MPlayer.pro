f550fc30e6d88bcfd5294bfd6a22430ab7c55ad2
Daniel Clarke <u8sand@gmail.com>
2014-08-22 09:40:47 -0400

    Added adjusting of version support in AboutDialog
    Set version to 1.9.8 see Baka-MPlayer.pro

--- src/Baka-MPlayer.pro
+++ src/Baka-MPlayer.pro
@@ -9,6 +9,7 @@ QT       += core gui network
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 TARGET = Baka-MPlayer
+DEFINES += 'BAKA_MPLAYER_VERSION=\\"1.9.8\\"'
 TEMPLATE = app
 
 SOURCES += main.cpp\
--- src/ui/aboutdialog.cpp
+++ src/ui/aboutdialog.cpp
@@ -1,11 +1,12 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 
-AboutDialog::AboutDialog(QWidget *parent) :
+AboutDialog::AboutDialog(QString version, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+    ui->versionLabel->setText("Baka MPlayer "+version);
 
     connect(ui->closeButton, SIGNAL(clicked()),
             this, SLOT(close()));
@@ -16,8 +17,8 @@ AboutDialog::~AboutDialog()
     delete ui;
 }
 
-void AboutDialog::about(QWidget *parent)
+void AboutDialog::about(QString version, QWidget *parent)
 {
-    AboutDialog dialog(parent);
+    AboutDialog dialog(version, parent);
     dialog.exec();
 }
