e7dd748ed41a557e202848d42e1dc32b05a47bae
Daniel Clarke <u8sand@gmail.com>
2014-08-24 16:02:06 -0400

    Reordered project file, changed settings storage
    On windows we load the .ini where the executable is.
    On linux we use Qt's code to save the settings in the .config directory.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,17 @@ Q_IMPORT_PLUGIN(qsvg)
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QSettings settings(SETTINGS_FILE, QSettings::IniFormat);
+
+    // todo: figure out how we want to deal with configuration on windows
+#if Q_OS_WIN
+    // saves to $(application directory)\${SETTINGS_FILE}.ini
+    QSettings settings(QApplication::applicationDirPath()+"\\"+SETTINGS_FILE, QSettings::IniFormat);
+#else
+    // saves to  ~/.config/${SETTINGS_FILE}.ini on linux
+    // saves to %APP_DATA%/${SETTINGS_FILE}.ini on windows
+    QSettings settings(QSettings::IniFormat, QSettings::UserScope,
+                       SETTINGS_FILE);
+#endif
 
     MainWindow w(&settings);
     w.show();
