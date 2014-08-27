281cddd141c43fa400a11675aab4baf9ace1b5dd
Daniel Clarke <u8sand@gmail.com>
2014-08-26 22:45:52 -0400

    Added basic implementation of PreferencesDialog
    Fixed other problems involving menu changes. There is still work to be done however because the updated settings need to be used.

---
+++ src/ui/preferencesdialog.h
@@ -0,0 +1,29 @@
+#ifndef PREFERENCESDIALOG_H
+#define PREFERENCESDIALOG_H
+
+#include <QDialog>
+#include <QSettings>
+
+namespace Ui {
+class PreferencesDialog;
+}
+
+class PreferencesDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PreferencesDialog(QSettings *settings, QWidget *parent = 0);
+    ~PreferencesDialog();
+
+    static void showPreferences(QSettings *settings, QWidget *parent = 0);
+
+protected slots:
+    void ChangeScreenshotLocation();
+
+private:
+    Ui::PreferencesDialog *ui;
+    QSettings *settings;
+};
+
+#endif // PREFERENCESDIALOG_H
