b1fe9217f83c602b209eb126d17b6f6ec62664ea
Daniel Clarke <u8sand@gmail.com>
2014-08-28 23:07:53 -0400

    Fixed DimLight accepting focus.

--- src/ui/lightdialog.cpp
+++ src/ui/lightdialog.cpp
@@ -3,12 +3,16 @@
 LightDialog::LightDialog(QWidget *parent) :
     QDialog(parent)
 {
+    // yes, the order here is strange. it doesn't work any other way.
     setWindowState(Qt::WindowFullScreen);
     setAttribute(Qt::WA_TranslucentBackground);
-    setWindowOpacity(.4);
+    setWindowOpacity(.6);
     setAttribute(Qt::WA_ShowWithoutActivating);
     setWindowFlags(Qt::Tool |
                    Qt::FramelessWindowHint |
                    Qt::WindowDoesNotAcceptFocus);
-    setStyleSheet("background-color: rgb(100,100,100);");
+    setStyleSheet("background-color: rgb(255,255,255);");
+    show();
+    hide();
+    setWindowFlags(windowFlags() | Qt::WindowTransparentForInput);
 }
