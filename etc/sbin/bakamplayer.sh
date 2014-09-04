8d850bc13765572de7492c729ef2b1873e6aa83e
Daniel Clarke <u8sand@gmail.com>
2014-09-04 19:05:45 -0400

    Minor changes related to the cross-platform build script

--- etc/sbin/bakamplayer.sh
+++ etc/sbin/bakamplayer.sh
@@ -6,7 +6,7 @@ export PATH=$(pwd)/mxe/usr/bin/:$PATH
 # get bakamplayer
 git clone https://github.com/u8sand/Baka-MPlayer.git
 
-# if you're mxe toolchain is only there for one architecture,
+# if your mxe toolchain is only there for one architecture,
 #  comment out this block, the other architecture's block, and
 #	  the cd command of your architecture
 # copy and make two folders: .x86_64, .i686
@@ -14,12 +14,13 @@ cp Baka-MPlayer{,.i686} -r
 mv Baka-MPlayer{,.x86_64}
 
 # compile i686
-cd "Baka-MPlayer.i686/Baka MPlayer"
-i686-w64-mingw32-qmake-qt5 .
+cd "Baka-MPlayer.i686/"
+i686-w64-mingw32-qmake-qt5 src/Baka-MPlayer.pro
 make
 cd ..
 
 # compile x86_64
-x86_64-w64-mingw32-qmake-qt5 .
+cd "Baka-MPlayer.x84_64/"
+x86_64-w64-mingw32-qmake-qt5 src/Baka-MPlayer.pro
 make
 cd ..
