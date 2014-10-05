5797a232197f7485aa4f2873925e9fa904681809
Daniel Clarke <u8sand@gmail.com>
2014-10-05 10:51:24 -0400

    Fixed some strange behavior with lastFile
    Files that were deleted would attempt to get loaded; this firstly would load their directory and then cause an mpv error. To prevent this I 1. made sure the file existed before even attempting to load the playlist and 2. checked if the lastFile existed before setting it from settings.

--- src/mpvhandler.cpp
+++ src/mpvhandler.cpp
@@ -129,9 +129,21 @@ bool MpvHandler::event(QEvent *event)
     return QObject::event(event);
 }
 
+bool MpvHandler::FileExists(QString f)
+{
+    QRegExp rx("^(https?://.+\\.[a-z]+)", Qt::CaseInsensitive);
+    if(rx.indexIn(f) != -1) // web url
+        return true;
+    return QFile(f).exists();
+}
+
 void MpvHandler::LoadSettings(QSettings *settings)
 {
-    setLastFile(settings->value("mpv/lastFile", "").toString());
+    QString lf = settings->value("mpv/lastFile", "").toString();
+    if(FileExists(lf)) // make sure the last file exists
+        setLastFile(lf);
+    else
+        setLastFile("");
     ShowAllPlaylist(settings->value("mpv/showAll", false).toBool());
     ScreenshotFormat(settings->value("mpv/screenshotFormat", "jpg").toString());
     ScreenshotTemplate(settings->value("mpv/screenshotTemplate", "screenshot%#04n").toString());
@@ -157,7 +169,8 @@ void MpvHandler::SaveSettings(QSettings *settings)
 
 void MpvHandler::LoadFile(QString f)
 {
-    if(f == "") return; // ignore empty file name
+    if(f == "") // ignore empty file name
+        return;
 
     int i;
     QRegExp rx("^(https?://.+\\.[a-z]+)", Qt::CaseInsensitive);
@@ -173,6 +186,8 @@ void MpvHandler::LoadFile(QString f)
     else // local file
     {
         QFileInfo fi(f);
+        if(!fi.exists()) // ignore if file doesn't exist
+            return;
         if(path != QString(fi.absolutePath()+"/") || // path is the same
           (i = playlist.indexOf(fi.fileName())) == -1) // file doesn't exists in the list
         {
