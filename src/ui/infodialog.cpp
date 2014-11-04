7cf2f13bb6b4f2cfc3646ce71fb3d63768b08e9d
Daniel Clarke <u8sand@gmail.com>
2014-11-04 10:20:14 -0500

    Failed to fix seekbar last commit--now it is
    Also updated media-info; centering headers.

--- src/ui/infodialog.cpp
+++ src/ui/infodialog.cpp
@@ -17,6 +17,8 @@ InfoDialog::InfoDialog(const QString &fileName, const Mpv::FileInfo &_fileInfo, 
     connect(ui->closeButton, SIGNAL(clicked()),
             this, SLOT(close()));
 
+    QTableWidgetItem *item;
+
     QFileInfo fi(fileName);
     QList<QPair<QString, QString>> items = {
         {"File name", fi.fileName()},
@@ -24,30 +26,42 @@ InfoDialog::InfoDialog(const QString &fileName, const Mpv::FileInfo &_fileInfo, 
         {"File size", HumanSize(fi.size())},
         {"Date created", fi.created().toString()},
         {"Media length", FormatTime(fileInfo.length, fileInfo.length)},
-        {"[Video]",""},
+        {"[Video]", QString()},
         {"Video codec", fileInfo.video_params.codec},
         {"Video format", fileInfo.video_params.format},
         {"Video bitrate", fileInfo.video_params.bitrate},
         {"Video dimensions", QString::number(fileInfo.video_params.width)+" x "+QString::number(fileInfo.video_params.height)},
-        {"[Audio]",""},
+        {"[Audio]", QString()},
         {"Audio codec", fileInfo.audio_params.codec},
         {"Audio format", fileInfo.audio_params.format},
         {"Audio bitrate", fileInfo.audio_params.bitrate},
         {"Audio samplerate", fileInfo.audio_params.samplerate},
         {"Audio channels", fileInfo.audio_params.channels}
     };
+
     ui->infoWidget->setRowCount(items.length());
     int r = 0;
     for(auto &iter : items)
     {
-        ui->infoWidget->setItem(r, 0, new QTableWidgetItem(iter.first));
-        ui->infoWidget->setItem(r, 1, new QTableWidgetItem(iter.second));
+        if(iter.second == QString())
+        {
+            item = new QTableWidgetItem(iter.first);
+            item->setTextAlignment(Qt::AlignCenter);
+            ui->infoWidget->setItem(r, 0, item);
+        }
+        else
+        {
+            ui->infoWidget->setItem(r, 0, new QTableWidgetItem(iter.first));
+            ui->infoWidget->setItem(r, 1, new QTableWidgetItem(iter.second));
+        }
         r++;
     }
     if(fileInfo.tracks.length() > 0)
     {
         ui->infoWidget->setRowCount(ui->infoWidget->rowCount()+1+fileInfo.tracks.length());
-        ui->infoWidget->setItem(r++, 0, new QTableWidgetItem("[Track List]"));
+        item = new QTableWidgetItem("[Track List]");
+        item->setTextAlignment(Qt::AlignCenter);
+        ui->infoWidget->setItem(r++, 0, item);
         for(auto &track : fileInfo.tracks)
         {
             ui->infoWidget->setItem(r, 0, new QTableWidgetItem(QString::number(track.id)));
@@ -58,7 +72,10 @@ InfoDialog::InfoDialog(const QString &fileName, const Mpv::FileInfo &_fileInfo, 
     if(fileInfo.chapters.length() > 0)
     {
         ui->infoWidget->setRowCount(ui->infoWidget->rowCount()+1+fileInfo.tracks.length());
-        ui->infoWidget->setItem(r++, 0, new QTableWidgetItem("[Chapter List]"));
+
+        item = new QTableWidgetItem("[Chapter List]");
+        item->setTextAlignment(Qt::AlignCenter);
+        ui->infoWidget->setItem(r++, 0, item);
         for(auto &chapter : fileInfo.chapters)
         {
             ui->infoWidget->setItem(r, 0, new QTableWidgetItem(chapter.title));
