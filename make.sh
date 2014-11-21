11697843179aae1a21cf106cc046b50525f94683
Daniel Clarke <u8sand@gmail.com>
2014-11-20 21:51:26 -0500

    Modified make.sh to build for 32 or 64 bit

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ninja=1
+arch=0
 
 hash cmake 2>/dev/null || { echo >&2 "Cannot find cmake."; exit 1; }
 hash ninja 2>/dev/null || { echo >&2 "Cannot find ninja."; ninja=0; }
@@ -8,12 +9,32 @@ if [[ $ninja == 0 ]]; then
 	hash make 2>/dev/null || { echo >&2 "Cannot find make."; exit 1; }
 fi
 
-mkdir -p build
-cd build
+if [[ $1 == "i686" ]]; then
+	arch=32
+elif [[ $1 == "x86_64" ]]; then
+	arch=64
+fi
+
 if [[ $ninja == 1  ]]; then
-	cmake -G Ninja ..
+	if [[ $arch -ne 0 ]]; then
+		mkdir -p build-$arch
+		cd build-$arch
+		cmake -G Ninja CMAKE_C_FLAGS=-m$arch -DCMAKE_CXX_FLAGS=-m$arch ..
+	else
+		mkdir -p build
+		cd build
+		cmake -G Ninja ..
+	fi
 	ninja
 else
-	cmake ..
+	if [[ $arch -ne 0 ]]; then
+		mkdir -p build-$arch
+		cd build-$arch
+		cmake CMAKE_C_FLAGS=-m$arch -DCMAKE_CXX_FLAGS=-m$arch ..
+	else
+		mkdir -p build
+		cd build
+		cmake ..
+	fi
 	make
 fi
