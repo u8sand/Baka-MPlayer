8ca08b0886f5a5d6009679ea71f568ed6984d79f
Daniel Clarke <u8sand@gmail.com>
2015-01-02 21:59:43 -0500

    Removed unnecessary `;`s

--- etc/sbin/create_windows_release.sh
+++ etc/sbin/create_windows_release.sh
@@ -4,9 +4,9 @@ function build_arch {
   log=build-$1.log
   echo `date` > $log
   echo "Building mpv.$1..." | tee -a $log
-  ./mpv.sh $1 >> $log 2>> $log;
+  ./mpv.sh $1 >> $log 2>> $log
   echo "Building baka-mplayer.$1..." | tee -a $log
-  ./bakamplayer.sh $1 >> $log 2>> $log;
+  ./bakamplayer.sh $1 >> $log 2>> $log
   echo "Compressing baka-mplayer.$1 with upx..." | tee -a $log
   upx Baka-MPlayer.$1/build/baka-mplayer.exe -o baka-mplayer-$1.exe >> $log 2>> $log
 }
