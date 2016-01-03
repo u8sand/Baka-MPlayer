7ac307344fb6c6658fa3ac47f1accc23aff86b2d
Daniel Clarke <u8sand@gmail.com>
2016-01-03 15:56:08 -0500

    Some changes in an attempt to get this to work

--- travis.sh
+++ travis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install libqt5x11extras5-dev libmpv-dev
 mkdir build
 cd build
 qmake-qt5 ../src/Baka-MPlayer.pro CONFIG+=release CONFIG+=install_translations -spec linux-g++
