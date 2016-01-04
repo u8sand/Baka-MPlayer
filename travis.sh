0e5a2f8381fad28b2e652c7b05668592dafc3629
Daniel Clarke <u8sand@gmail.com>
2016-01-04 08:06:11 -0500

    Building mpv ourselves in travis.sh

--- travis.sh
+++ travis.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-mkdir build
-cd build
-qmake-qt5 ../src/Baka-MPlayer.pro CONFIG+=release CONFIG+=install_translations -spec linux-g++
-make -j $(grep -c ^processor /proc/cpuinfo)
+mkdir -p build/mpv
+cd build/mpv
 
+git clone https://github.com/mpv-player/mpv-build.git
+cd mpv-build
+echo --disable-pdf-build --enable-libmpv-shared > mpv_options
+./update
+# install mpv-dependencies
+mk-build-deps -s sudo -i
+dpkg-buildpackage -uc -us -b -j${JOBS}
+# install mpv
+sudo dpkg -i ../mpv*.deb
+cd ..
+
+mkdir -p baka-mplayer
+cd baka-mplayer
+qmake-qt5 ../../src/Baka-MPlayer.pro CONFIG+=release CONFIG+=install_translations -spec linux-g++
+make -j $(grep -c ^processor /proc/cpuinfo)
