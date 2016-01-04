a69f963e7896e261348a0f6515d8bb8987dacf95
Daniel Clarke <u8sand@gmail.com>
2016-01-04 08:25:07 -0500

    Trying to just install libmpv from apt-get now that the other dep is whitelisted

--- travis.sh
+++ travis.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-mkdir -p build/mpv
-cd build/mpv
-
-git clone https://github.com/mpv-player/mpv-build.git
-cd mpv-build
-echo --disable-pdf-build --enable-libmpv-shared > mpv_options
-./update
-# install mpv-dependencies
-mk-build-deps -s sudo -i
-dpkg-buildpackage -uc -us -b -j${JOBS}
-# install mpv
-sudo dpkg -i ../mpv*.deb
-cd ..
-
-mkdir -p baka-mplayer
-cd baka-mplayer
+mkdir -p build
+cd build
 qmake-qt5 ../../src/Baka-MPlayer.pro CONFIG+=release CONFIG+=install_translations -spec linux-g++
 make -j $(grep -c ^processor /proc/cpuinfo)
